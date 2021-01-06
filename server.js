8cc574ad86dd94b36976755ffbfb8ecd2fc40cac
CJ Clark <cjclark@CJs-MacBook-Air.local>
2021-01-06 17:42:12 -0600

    catch all route

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const express = require('express');
 const app = require('./dog_api/app')
 
 app.use(express.static('build'));
+
+app.get('*', (req, res)=> {
+    res.sendFile(path.resolve(`${__dirname}/build/index.html`));
+  });
